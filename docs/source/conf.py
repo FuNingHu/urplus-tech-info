53ee1a3b0b10dcee700a4835d74c12982af35c94
FuNIngHu <funing_hu@163.com>
2025-12-09 16:21:27 +0800

    update version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -15,7 +15,9 @@ release = '0.1.2'
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
 
-extensions = []
+extensions = [
+    'sphinx_multiversion',
+]
 
 templates_path = ['_templates']
 exclude_patterns = []
@@ -38,3 +40,28 @@ html_theme_options = {
 
 # Logo
 html_logo = 'images/ur_logo.png'
+
+# -- Sphinx Multiversion Configuration ---------------------------------------
+# Whitelist pattern for tags (e.g., v0.1.0, v1.0.0)
+smv_tag_whitelist = r'^v\d+\.\d+\.\d+$'
+
+# Whitelist pattern for branches
+smv_branch_whitelist = r'^(main|master)$'
+
+# Pattern for released versions
+smv_released_pattern = r'^tags/v\d+\.\d+\.\d+$'
+
+# Format for versioned output directories
+smv_outputdir_format = '{ref.name}'
+
+# Show warning banner for old versions
+smv_latest_version = 'main'
+
+# HTML context for version selector
+html_context = {
+    'display_github': True,
+    'github_user': 'funinghu',
+    'github_repo': 'urplus-tech-info',
+    'github_version': 'main',
+    'conf_py_path': '/docs/source/',
+}
