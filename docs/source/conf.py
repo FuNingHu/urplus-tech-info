8f843301665f68d58d71d8c94a845e61165914b7
FuNIngHu <funing_hu@163.com>
2025-12-09 17:06:45 +0800

    update major version

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -10,7 +10,7 @@ project = 'UR+ Tech Info'
 copyright = '2025, Universal Robots A/S'
 author = 'UR Development Consultant Team'
 version = '0.1'
-release = '0.1.2'
+release = '0.2.2'
 
 # -- General configuration ---------------------------------------------------
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#general-configuration
